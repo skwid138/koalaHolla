2cf969e90bf0bbaa609db97a61415f187816b5c9
Hunter Rancourt <skwid138@gmail.com>
2017-09-13 16:25:08 -0500

    base mode working

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -6,31 +6,47 @@ $( document ).ready( function(){
   getKoalas();
 
   // add koala button click
-  $( '#addButton' ).on( 'click', function(){
-    console.log( 'in addButton on click' );
+  $( '#addButton' ).on( 'click', function () {
     // get user input and put in an object
     // NOT WORKING YET :(
     // using a test object
     var objectToSend = {
-      name: 'testName',
-      age: 'testName',
-      gender: 'testName',
-      readyForTransfer: 'testName',
-      notes: 'testName',
+      name: $('#nameIn').val(),
+      age: $('#ageIn').val(),
+      gender: $('#genderIn').val(),
+      readyForTransfer: $('#readyForTransferIn').val(),
+      notes: $('#notesIn').val(),
     };
     // call saveKoala with the new obejct
     saveKoala( objectToSend );
+    //clear user input
+    $('#nameIn').val('');
+    $('#ageIn').val('');
+    $('#genderIn').val('');
+    $('#readyForTransferIn').val('');
+    $('#notesIn').val('');
   }); //end addButton on click
 }); // end doc ready
 
 function getKoalas(){
   console.log( 'in getKoalas' );
+  $('#viewKoalas').empty();
   // ajax call to server to get koalas
   $.ajax({
     url: '/koalas',
     type: 'GET',
     success: function( data ){
       console.log( 'got some koalas: ', data );
+      var $kol = $('#viewKoalas');
+      for (i = 0; i < data.length; i++) {
+       var $tr = $('<tr>');
+        $tr.append('<td>' + data[i].name + '</td>');
+        $tr.append('<td>' + data[i].age + '</td>');
+        $tr.append('<td>' + data[i].gender + '</td>');
+        $tr.append('<td>' + data[i].transfer + '</td>');
+        $tr.append('<td>' + data[i].notes + '</td>');
+        $kol.append($tr);
+      }//END for loop
     } // end success
   }); //end ajax
   // display on DOM with buttons that allow edit of each
@@ -45,6 +61,7 @@ function saveKoala( newKoala ){
     data: newKoala,
     success: function( data ){
       console.log( 'got some koalas: ', data );
+      getKoalas();
     } // end success
   }); //end ajax
 }
--- server/routes/koala.js
+++ server/routes/koala.js
@@ -25,4 +25,33 @@
      });//END connect
  });//END GET 
 
+ router.post('/', function (req, res) {
+     var name = req.body.name;
+     var age = req.body.age;
+     var gender = req.body.gender;
+     var transfer = req.body.readyForTransfer;
+     var notes = req.body.notes;
+     pool.connect(function (err, client, done) {
+         if (err) {
+             console.log('connection error:', err);
+             res.sendStatus(500);
+             done();
+         }//END if err
+         else {
+             var queryString = 'INSERT INTO holla (name, age, gender, transfer, notes) VALUES ($1, $2, $3, $4, $5)';
+             var values = [name, age, gender, transfer, notes];
+             client.query(queryString, values, function (qErr, results) {
+                 if (qErr) {
+                     console.log('query error:', qErr);
+                     res.sendStatus(500);
+                 }//END if qErr
+                 else {
+                     res.sendStatus(201);
+                 }//END else
+                 done();
+             });//END client.query
+         }//END else
+     });//END connect
+ });//END GET 
+
  module.exports = router;
